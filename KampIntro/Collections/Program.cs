62c4ae86d12e2e6dc4221e5f05d305cf773fd4c2
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-06-23 21:29:29 +0300

    feat: we have introduced generic types

---
+++ KampIntro/Collections/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] Names = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            //Console.WriteLine(Names[0]);
+            //Console.WriteLine(Names[1]);
+            //Console.WriteLine(Names[2]);
+            //Console.WriteLine(Names[3]);
+
+            List<string> Names2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            foreach (string name in Names2)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
