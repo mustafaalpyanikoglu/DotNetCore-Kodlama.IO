056a0a1974c6f3de7efd49a39076e762ac2fffc2
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-07-02 18:11:12 +0300

    feat: homework six completed

---
+++ KampIntro/HomeworkSix.sql
@@ -0,0 +1,5 @@
+﻿select p.ProductName,sum(od.UnitPrice*od.Quantity) as KazanılanMiktar from Products p 
+inner join [Order Details] od on p.ProductID=od.ProductID
+inner join Orders o on o.OrderID=od.OrderID 
+group by p.ProductName
+order by KazanılanMiktar desc
