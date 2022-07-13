b8f4166b44f7baa5fdca6287c636c8084592cb8f
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-07-13 13:35:03 +0300

    feat: end of the ninth day

--- KampIntro/MyFinalProject/DataAccess/Abstract/IProductDal.cs
+++ KampIntro/MyFinalProject/DataAccess/Abstract/IProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using Core.DataAccess;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace DataAccess.Abstract
 {
     public interface IProductDal:IEntityRepository<Product>
     {
+        List<ProductDetailDto> GetProductDetails();
     }
 }
 //Code Refactoring
