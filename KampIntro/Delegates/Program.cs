b3652dd2aae2327c6aea969462c37711c9907509
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-07-21 11:06:33 +0300

    feat: Delegates and Events

---
+++ KampIntro/Delegates/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Delegates
+{
+    public delegate void MyDelegate();
+    public delegate void MyDelegate2(string text);
+    public delegate int MyDelegate3(int number1, int number2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            MyDelegate myDelegate = customerManager.SendMessage;
+            myDelegate += customerManager.ShowAlert;
+            myDelegate -= customerManager.SendMessage;
+            myDelegate();
+
+            MyDelegate2 myDelegate2 = customerManager.SendMessage2;
+            myDelegate2 += customerManager.ShowAlert2;
+            myDelegate2("Hello");
+
+            Math math = new Math();
+            MyDelegate3 myDelegate3 = math.Sum;
+            var conslusion = myDelegate3(2, 3);
+            Console.WriteLine(conslusion);
+        }
+    }
+
+    public class CustomerManager
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Hello!");
+        }
+        public void ShowAlert()
+        {
+            Console.WriteLine("Be careful!");
+        }
+
+        public void SendMessage2(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public void ShowAlert2(string allert)
+        {
+            Console.WriteLine(allert);
+        }
+    }
+
+    public class Math
+    {
+        public int Sum(int number1,int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
