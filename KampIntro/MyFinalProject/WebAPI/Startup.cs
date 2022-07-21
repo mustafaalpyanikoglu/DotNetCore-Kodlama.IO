ee80607a3ebd3ceb44b608f7e921a608191ba7dc
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-07-21 16:25:46 +0300

    feat: the twelfth day is complete

--- KampIntro/MyFinalProject/WebAPI/Startup.cs
+++ KampIntro/MyFinalProject/WebAPI/Startup.cs
@@ -32,8 +32,10 @@ namespace WebAPI
         {
             //AOP
             services.AddControllers();
-            services.AddSingleton<IProductService, ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            
+            //services.AddSingleton<IProductService, ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
