b40313e473fbe19a3d26ac1508be3339c72877bf
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-07-26 10:54:22 +0300

    feat: second commit on the fourteenth day

---
+++ KampIntro/MyFinalProject/Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        //password için hash oluşturduk
+        public static void CreatePasswordHash
+            (string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac= new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;   //algoritma bir key değeri oluşturur
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            }
+        }
+
+        //sonradan girilen passwordun veri kaynağındaki passwordSalt'a göre eşleşip eşleşmediğinin kontrolü yapılır
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if(computedHash[i]!=passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
