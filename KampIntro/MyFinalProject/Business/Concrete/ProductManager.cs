ad8b2990fd0d2e08b3531ba5a5990d77b76bf5b9
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-07-29 15:20:24 +0300

    feat: first part of the fifteenth day lesson

--- KampIntro/MyFinalProject/Business/Concrete/ProductManager.cs
+++ KampIntro/MyFinalProject/Business/Concrete/ProductManager.cs
@@ -28,7 +28,7 @@ namespace Business.Concrete
             _categoryService = categoryService;
         }
 
-        [SecuredOperation("product.add")]
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
